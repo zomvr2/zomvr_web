b70027f52eb9aff32082d223accdd09f793f82d7
zomvr <60558284+zomvr2@users.noreply.github.com>
2021-05-19 21:13:30 -0400

    CREATED

---
+++ public/script.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("toggle_mode");
+let header = document.getElementById("header");
+let body = document.body;
+let container = document.getElementById("container");
+let image = document.getElementById("profile_picture");
+
+image.setAttribute("draggable", false)
